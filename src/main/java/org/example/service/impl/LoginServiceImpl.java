83f4e435101e1afe812982b87e205960bfcdd947
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-09 18:58:49 +0530

    removed unecessary comments

--- src/main/java/org/example/service/impl/LoginServiceImpl.java
+++ src/main/java/org/example/service/impl/LoginServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.dto.LogInDto;
 import org.example.entity.LogInEntity;
 import org.example.repository.LoginRepository;
@@ -9,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class LoginServiceImpl implements LoginService {
     private final LoginRepository loginRepository;
     private final ModelMapper modelMapper;
@@ -19,6 +21,7 @@ public class LoginServiceImpl implements LoginService {
 
     @Override
     public Boolean validateLogin(LogInDto logInDto) {
+        log.info(logInDto.toString());
         LogInEntity logInEntity = modelMapper.map(logInDto, LogInEntity.class);
         return loginRepository.existsAllByUserNameAndPassword(logInEntity.getUserName(), logInEntity.getPassword());
     }
